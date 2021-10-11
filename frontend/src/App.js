e52e317a4ded2b4d5ca674bc9fdae00805282ee1
jacksonwun <jacksonwun@gmail.com>
2021-10-12 01:46:44 +0800

    update frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import {
+  Switch,
+  Route,
+  useLocation
+} from 'react-router-dom';
+
+import './css/style.scss';
+
+import { focusHandling } from 'cruip-js-toolkit';
+import './charts/ChartjsConfig';
+
+// Import pages
+import Dashboard from './pages/Dashboard';
+import Warrant from './pages/Warrant';
+// import Chat from './pages/websocket';
+
+function App() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+    focusHandling('outline');
+  }, [location.pathname]); // triggered on route change
+
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Dashboard />
+        </Route>
+        <Route exact path="/warrant">
+          <Warrant />
+        </Route>
+        {/* <Route exact path="/websocket">
+          <Chat />
+        </Route> */}
+      </Switch>
+    </>
+  );
+}
+
+export default App;
