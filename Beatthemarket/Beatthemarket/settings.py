09aed2ab042d8605c059ea2983a9d48296b5ffbf
jacksonwun <jacksonwun@gmail.com>
2021-10-13 01:07:30 +0800

    update

--- Beatthemarket/Beatthemarket/settings.py
+++ Beatthemarket/Beatthemarket/settings.py
@@ -124,13 +124,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Asia/Hong_Kong'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
--- Beatthemarket/algoTrade/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AlgotradeConfig(AppConfig):
-    name = 'algoTrade'
